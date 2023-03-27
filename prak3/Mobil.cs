1bcda90cfaff3e463dff6115e36e8ce0ecc5271a
linndand <linnda.p.n@students.amikom.ac.id>
2023-03-27 08:59:53 +0700

    praktikum3

---
+++ prak3/Mobil.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak3
+{
+    public class Mobil
+    {
+        public string Warna { get; set; }
+        public string merek { get; set; }
+        public string model { get; set; }
+        public string tahun_keluaran { get; set; }
+        public string kecepatan { get; set; }
+        public string suara { get; set; }
+        public string jumlah_pintu { get; set; }
+        public string Tampilkan_info { get; set; }
+
+        public void warna()
+        {
+            Console.WriteLine("warna : {0}", Warna);
+        }
+        public void merk()
+        {
+            Console.WriteLine("merek : {0}", merek);
+        }
+        public void models()
+        {
+            Console.WriteLine("model : {0}", model);
+        }
+        public void year()
+        {
+            Console.WriteLine("tahun keluaran : {0}", tahun_keluaran);
+        }
+        public void gas()
+        {
+            Console.WriteLine($"Mobil {model}, berjalan dengan kecepatan : {kecepatan}", kecepatan);
+        }
+        public void klakson()
+        {
+            Console.WriteLine("suara klakson : {0}", suara);
+        }
+        public void pintu()
+        {
+            Console.WriteLine("jumlah pintu : {0}", jumlah_pintu);
+        }
+        public void tampilkan_info()
+        {
+            Console.WriteLine($"Mobil saya berwarna {Warna}, merek {merek}, model {model}, keluaran tahun {tahun_keluaran}, dengan jumlah pintu sebanyak {jumlah_pintu}", Tampilkan_info);
+        }
+           
+    }
+}
---
+++ prak3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil = new Mobil();
+
+            mobil.Warna = "Hitam";
+            mobil.merek = "Daihaha";
+            mobil.model = "Cylock";
+            mobil.tahun_keluaran = "2018";
+            mobil.kecepatan = "60 km/jam";
+            mobil.suara = "TINTINNNN";
+            mobil.jumlah_pintu = "5\n";
+            mobil.Tampilkan_info = "info";
+
+            mobil.warna();
+            mobil.merk();
+            mobil.models();
+            mobil.year();
+            mobil.gas();
+            mobil.klakson();
+            mobil.pintu();
+            mobil.tampilkan_info();
+
+            Console.ReadKey();
+        }
+    }
+}
